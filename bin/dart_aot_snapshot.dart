d66778c579ac245918ce2662bbe701c95b156295
aospiridonov <aospiridonov@gmail.com>
2023-02-23 14:46:57 +0300

    init dart project

---
+++ bin/dart_aot_snapshot.dart
@@ -0,0 +1,5 @@
+import 'package:dart_aot_snapshot/dart_aot_snapshot.dart' as dart_aot_snapshot;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_aot_snapshot.calculate()}!');
+}
---
+++ test/dart_aot_snapshot_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_aot_snapshot/dart_aot_snapshot.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
